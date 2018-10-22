c68f13bf4a1f6c906aa1539688c9ca5ffeb2ea9e
loordy <loordy@live.ru>
2018-10-22 09:59:27 +0500

    infyom

--- app/Http/Controllers/AppBaseController.php
+++ app/Http/Controllers/AppBaseController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Utils\ResponseUtil;
+use InfyOm\Generator\Utils\ResponseUtil;
 use Response;
 
-
+/**
+ * @SWG\Swagger(
+ *   basePath="/api/v1",
+ *   @SWG\Info(
+ *     title="Laravel Generator APIs",
+ *     version="1.0.0",
+ *   )
+ * )
+ * This class should be parent class for other API controllers
+ * Class AppBaseController
+ */
 class AppBaseController extends Controller
 {
     public function sendResponse($result, $message)
